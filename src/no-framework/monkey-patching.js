d4395b8b8608a840f88fc79060e9484824332b26
Takuya Kikuchi <takuyakikuch@gmail.com>
2022-05-13 09:56:56 +0900

    lesson 2: mock with monkey-patching

--- src/no-framework/monkey-patching.js
+++ src/no-framework/monkey-patching.js
@@ -1,3 +1,16 @@
 const assert = require('assert')
 const thumbWar = require('../thumb-war')
 const utils = require('../utils')
+
+// Monkey patching
+// Mocking the utils.getWinner function by overrding the property
+utils.getWinner = (p1, p2) => p1;
+
+const winner = thumbWar('TK', 'Kent C. Dodds');
+
+// https://nodejs.org/api/assert.html#assertstrictequalactual-expected-message
+assert.strictEqual(winner, 'TK');
+
+// Clean up mock function to avoid impacting other tests.
+const originalGetWinner = utils.getWinner;
+utils.getWinner = originalGetWinner;
