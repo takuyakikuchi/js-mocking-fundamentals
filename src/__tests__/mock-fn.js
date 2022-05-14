19bc7b34353a94f13ec74a644bd841873b7a61ad
Takuya Kikuchi <takuyakikuch@gmail.com>
2022-05-14 09:31:55 +0900

    before the lesson 3

--- src/__tests__/mock-fn.js
+++ src/__tests__/mock-fn.js
@@ -7,22 +7,6 @@ test('returns winner', () => {
 
   const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
   expect(winner).toBe('Kent C. Dodds')
-  expect(utils.getWinner.mock.calls).toEqual([
-    ['Kent C. Dodds', 'Ken Wheeler'],
-    ['Kent C. Dodds', 'Ken Wheeler']
-  ])
-  // could also do these assertions:
-  expect(utils.getWinner).toHaveBeenCalledTimes(2)
-  expect(utils.getWinner).toHaveBeenNthCalledWith(
-    1,
-    'Kent C. Dodds',
-    'Ken Wheeler'
-  )
-  expect(utils.getWinner).toHaveBeenNthCalledWith(
-    2,
-    'Kent C. Dodds',
-    'Ken Wheeler'
-  )
 
   // cleanup
   utils.getWinner = originalGetWinner
