322799b78bef485628db3b2e80cd950c875cbc15
Takuya Kikuchi <takuyakikuch@gmail.com>
2022-05-16 08:48:44 +0900

    lesson 3: Mock with jest

--- src/__tests__/mock-fn.js
+++ src/__tests__/mock-fn.js
@@ -3,10 +3,24 @@ const utils = require('../utils')
 
 test('returns winner', () => {
   const originalGetWinner = utils.getWinner
-  utils.getWinner = jest.fn((p1, p2) => p1)
+
+  // Mock function
+  utils.getWinner = jest.fn((player1, player2) => player1)
 
   const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
-  expect(winner).toBe('Kent C. Dodds')
+  
+  // console.log(utils.getWinner)
+  
+  // Testing number of calls and its arguments
+  // expect(utils.getWinner).toHaveBeenCalledTimes(2)
+  // expect(utils.getWinner).toHaveBeenCalledWith('Kent C. Dodds', 'Ken Wheeler')
+  // expect(utils.getWinner).toHaveBeenNthCalledWith(1,'Kent C. Dodds', 'Ken Wheeler')
+  // expect(utils.getWinner).toHaveBeenNthCalledWith(2,'Kent C. Dodds', 'Ken Wheeler')
+  expect(utils.getWinner.mock.calls).toEqual([
+    ['Kent C. Dodds', 'Ken Wheeler'],
+    ['Kent C. Dodds', 'Ken Wheeler'],
+  ])
+
 
   // cleanup
   utils.getWinner = originalGetWinner
