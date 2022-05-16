1dda0ea678df6db046dcc79ef466c51983718c6b
Takuya Kikuchi <takuyakikuch@gmail.com>
2022-05-16 09:18:28 +0900

    lesson 3: Mock with no-framework

--- src/no-framework/mock-fn.js
+++ src/no-framework/mock-fn.js
@@ -2,11 +2,33 @@ const assert = require('assert')
 const thumbWar = require('../thumb-war')
 const utils = require('../utils')
 
+/**
+ * 
+ * @param {function} implementation 
+ * @returns function that calls the implementation with all of the arguments.
+ */
+function fn(implementation) {
+  const mockFn = (...arguments) => {
+    mockFn.mock.calls.push(arguments)
+    return implementation(...arguments)
+  }
+
+  // All of the arguments that this function is called with.
+  mockFn.mock = {calls: []};
+
+  return mockFn
+}
+
 const originalGetWinner = utils.getWinner
-utils.getWinner = (p1, p2) => p1;
+utils.getWinner = fn((p1, p2) => p1)
 
 const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
 assert.strictEqual(winner, 'Kent C. Dodds')
+// https://nodejs.org/api/assert.html#assertdeepstrictequalactual-expected-message
+assert.deepStrictEqual(utils.getWinner.mock.calls, [
+  [ 'Kent C. Dodds', 'Ken Wheeler' ],
+  [ 'Kent C. Dodds', 'Ken Wheeler' ]
+])
 
 // cleanup
 utils.getWinner = originalGetWinner
