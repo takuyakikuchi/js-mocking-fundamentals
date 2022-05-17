c167c8bd0e492119b45916335279c69d89aba4c5
Takuya Kikuchi <takuyakikuch@gmail.com>
2022-05-18 08:42:34 +0900

    before the lesson 5

--- src/__tests__/inline-module-mock.js
+++ src/__tests__/inline-module-mock.js
@@ -1,20 +1,31 @@
 const thumbWar = require('../thumb-war')
-const utilsMock = require('../utils')
-
-jest.mock('../utils', () => {
-  return {
-    getWinner: jest.fn((p1, p2) => p1)
-  }
-})
+const utils = require('../utils')
 
 test('returns winner', () => {
+  /**
+   * jest.SpyOn(object, methodName)
+   * : Create a function(same as jest.fn()) and implement a call with object[methodName] passed as argument.
+   * (https://jestjs.io/ja/docs/jest-object#jestspyonobject-methodname)
+   */
+  jest.spyOn(utils, 'getWinner')
+
+  // Mock function
+  // utils.getWinner = jest.fn((player1, player2) => player1)
+  utils.getWinner.mockImplementation((player1, player2) => player1)
+
   const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
-  expect(winner).toBe('Kent C. Dodds')
-  expect(utilsMock.getWinner.mock.calls).toEqual([
+  
+  // Testing number of calls and its arguments
+  expect(utils.getWinner.mock.calls).toEqual([
     ['Kent C. Dodds', 'Ken Wheeler'],
-    ['Kent C. Dodds', 'Ken Wheeler']
+    ['Kent C. Dodds', 'Ken Wheeler'],
   ])
 
-  // cleanup
-  utilsMock.getWinner.mockReset()
+
+  /**
+   * mockFn.mockRestore()
+   * : Restore the original function.
+   * https://jestjs.io/ja/docs/mock-function-api#mockfnmockrestore
+   */
+  utils.getWinner.mockRestore()
 })
--- src/no-framework/inline-module-mock.js
+++ src/no-framework/inline-module-mock.js
@@ -1,32 +1,50 @@
-function fn(impl = () => {}) {
-  const mockFn = (...args) => {
-    mockFn.mock.calls.push(args)
-    return impl(...args)
-  }
-  mockFn.mock = {calls: []}
-  return mockFn
-}
-
-const utilsPath = require.resolve('../utils')
-require.cache[utilsPath] = {
-  id: utilsPath,
-  filename: utilsPath,
-  loaded: true,
-  exports: {
-    getWinner: fn((p1, p2) => p1)
-  }
-}
-
 const assert = require('assert')
 const thumbWar = require('../thumb-war')
 const utils = require('../utils')
 
+/**
+ * Mock function factory.
+ * @param {function} implementation 
+ * @returns function that calls the implementation with all of the arguments.
+ */
+function fn(implementation = () => {}) {
+  const mockFn = (...arguments) => {
+    mockFn.mock.calls.push(arguments)
+    return implementation(...arguments)
+  }
+
+  // All of the arguments that this function is called with.
+  mockFn.mock = {calls: []};
+
+  mockFn.mockImplementation = newImplementation => {implementation = newImplementation}
+
+  return mockFn
+}
+
+/**
+ * This function is responsible for tracking the originalValue.
+ * @param {Object} object 
+ * @param {String} methodName 
+ */
+function spyOn(object, methodName) {
+  const originalValue = object[methodName]
+
+  // Set an empty arrow function as a default implementation.
+  object[methodName] = fn()
+
+  object[methodName].mockRestore = () => {object[methodName] = originalValue}
+}
+
+spyOn(utils, 'getWinner')
+utils.getWinner.mockImplementation((p1, p2) => p1)
+
 const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
 assert.strictEqual(winner, 'Kent C. Dodds')
+// https://nodejs.org/api/assert.html#assertdeepstrictequalactual-expected-message
 assert.deepStrictEqual(utils.getWinner.mock.calls, [
   ['Kent C. Dodds', 'Ken Wheeler'],
   ['Kent C. Dodds', 'Ken Wheeler']
 ])
 
 // cleanup
-delete require.cache[utilsPath]
+utils.getWinner.mockRestore()
