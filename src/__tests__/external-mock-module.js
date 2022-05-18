a52f600fda61ce1c78b539ba32be2f8871865677
Takuya Kikuchi <takuyakikuch@gmail.com>
2022-05-19 08:29:56 +0900

    before the lesson 6

--- src/__tests__/external-mock-module.js
+++ src/__tests__/external-mock-module.js
@@ -1,16 +1,29 @@
 const thumbWar = require('../thumb-war')
-const utilsMock = require('../utils')
+const utils = require('../utils')
 
-jest.mock('../utils')
+/**
+ * Mock the entire module.
+ * @param {String} 'path to a module'
+ * @param {Function} 'a module factory function'
+ */
+jest.mock('../utils', () => {
+  return {
+    // Mock the getWinner function.
+    getWinner: jest.fn((p1, p2) => p1)
+  }
+})
 
 test('returns winner', () => {
   const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
   expect(winner).toBe('Kent C. Dodds')
-  expect(utilsMock.getWinner.mock.calls).toEqual([
+  // Testing number of calls and its arguments
+  expect(utils.getWinner.mock.calls).toEqual([
     ['Kent C. Dodds', 'Ken Wheeler'],
-    ['Kent C. Dodds', 'Ken Wheeler']
+    ['Kent C. Dodds', 'Ken Wheeler'],
   ])
 
-  // cleanup
-  utilsMock.getWinner.mockReset()
+
+  // Cleanup
+  // Reset the mock function to the initial state.
+  utils.getWinner.mockReset()
 })
--- src/no-framework/external-mock-module.js
+++ src/no-framework/external-mock-module.js
@@ -1,7 +1,37 @@
-require('../__no-framework-mocks__/utils') // prime the cache
+/**
+ * Mock function factory.
+ * @param {function} implementation 
+ * @returns function that calls the implementation with all of the arguments.
+ */
+ function fn(implementation = () => {}) {
+  const mockFn = (...arguments) => {
+    mockFn.mock.calls.push(arguments)
+    return implementation(...arguments)
+  }
+
+  // All of the arguments that this function is called with.
+  mockFn.mock = {calls: []};
+
+  mockFn.mockImplementation = newImplementation => {implementation = newImplementation}
+
+  return mockFn
+}
+
+// https://nodejs.org/api/modules.html#requireresolverequest-options
 const utilsPath = require.resolve('../utils')
-const mockUtilsPath = require.resolve('../__no-framework-mocks__/utils')
-require.cache[utilsPath] = require.cache[mockUtilsPath]
+/**
+ * Simulate mocking entire module with require.cache.
+ * https://nodejs.org/api/modules.html#requirecache
+ */
+require.cache[utilsPath] = {
+  id: utilsPath,
+  filename: utilsPath,
+  loaded: true,
+  exports: {
+    // Mock the getWinner function.
+    getWinner: fn((p1, p2) => p1)
+  }
+}
 
 const assert = require('assert')
 const thumbWar = require('../thumb-war')
@@ -9,6 +39,7 @@ const utils = require('../utils')
 
 const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
 assert.strictEqual(winner, 'Kent C. Dodds')
+// https://nodejs.org/api/assert.html#assertdeepstrictequalactual-expected-message
 assert.deepStrictEqual(utils.getWinner.mock.calls, [
   ['Kent C. Dodds', 'Ken Wheeler'],
   ['Kent C. Dodds', 'Ken Wheeler']
